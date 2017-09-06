950db29983d14805b0e72ac590cd925e59a4d108
chernyshof <dmitriy.chernyshof@gmail.com>
2017-09-06 11:01:49 +0300

    Add proptype for store

--- assets/app/config/Root.js
+++ assets/app/config/Root.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import { Provider, connect } from 'react-redux';
 import { Route, Switch } from 'react-router';
 import { ConnectedRouter as Router } from 'react-router-redux';
 import PropTypes from 'prop-types';
@@ -35,17 +35,19 @@ class Root extends Component {
     };
 
     return (
-      <div className="full-height">
-        <ErrorMessage />
-        <Router history={this.props.history}>
-          <Switch>
-            <MatchAuthenticated exact path="/" component={App} {...authProps} />
-            <RedirectAuthenticated path="/signup" component={Signup} {...authProps} />
-            <RedirectAuthenticated path="/login" component={Login} {...authProps} />
-            <Route component={NotFound} />
-          </Switch>
-        </Router>
-      </div>
+      <Provider store={this.props.store}>
+        <div className="full-height">
+          <ErrorMessage />
+          <Router history={this.props.history}>
+            <Switch>
+              <MatchAuthenticated exact path="/" component={App} {...authProps} />
+              <RedirectAuthenticated path="/signup" component={Signup} {...authProps} />
+              <RedirectAuthenticated path="/login" component={Login} {...authProps} />
+              <Route component={NotFound} />
+            </Switch>
+          </Router>
+        </div>
+      </Provider>
     );
   }
 }
@@ -57,6 +59,7 @@ Root.propTypes = {
   unauthenticate: PropTypes.func.isRequired,
   isAuthenticated: PropTypes.bool.isRequired,
   willAuthenticate: PropTypes.bool.isRequired,
+  store: PropTypes.object.isRequired,  // eslint-disable-line react/forbid-prop-types
 };
 
 const mapDispatchToProps = dispatch => ({
