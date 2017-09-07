6bd683c014c8dbd912df406e4cd35becb87dcdad
chernyshof <dmitriy.chernyshof@gmail.com>
2017-09-07 19:31:39 +0300

    Oops

--- config/prod.exs
+++ config/prod.exs
@@ -24,7 +24,7 @@ config :boilerplate, BoilerplateWeb.Endpoint,
 config :logger, level: :info
 
 # Configure database
-config :elephang, Boilerplate.Repo,
+config :boilerplate, Boilerplate.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
