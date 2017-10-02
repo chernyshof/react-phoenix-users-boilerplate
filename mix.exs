2bfec32a98fd21a23b8a8e9828bae41c06edf252
chernyshof <dmitriy.chernyshof@gmail.com>
2017-10-02 07:49:32 +0300

    elixir 1.5

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Boilerplate.Mixfile do
     [
       app: :boilerplate,
       version: "0.0.1",
-      elixir: "~> 1.4",
+      elixir: "~> 1.5",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
       start_permanent: Mix.env == :prod,
