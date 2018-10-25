49991528853fa236787d10250a67b90f8fd28110
chernyshof <dmitriy.chernyshof@gmail.com>
2018-10-25 14:33:48 +0300

    Small change

--- assets/app/components/Sign/Signup/SignupForm/index.js
+++ assets/app/components/Sign/Signup/SignupForm/index.js
@@ -84,7 +84,7 @@ const validate = (values) => {
   if (!values.username) {
     errors.username = 'Required';
   } else if (!usernameRegex.test(values.username)) {
-    errors.username = 'EN letters, digits, \'.\' and \'_\' are accepted';
+    errors.username = 'Only EN letters, digits, \'.\' and \'_\' are allowed';
   } else if (values.username.length < 1 || values.username.length > 26) {
     errors.username = 'Must be less than 27 characters';
   }
